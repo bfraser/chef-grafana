96e3fa69c088b277466c35cca054c4ac22f0ff32
Bill Fraser <fraser@pythian.com>
2014-10-02 23:35:49 -0400

    Fix author name :)

--- recipes/_apache.rb
+++ recipes/_apache.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: grafana
 # Recipe:: apache
 #
-# Copyright 2014, Jonathan Tron
+# Copyright 2014, Bill Fraser
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
